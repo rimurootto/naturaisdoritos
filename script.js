6fcdbee47ee0fe4ec21c34e29ea4751e2fbf4f1f
rimurootto <160866529+rimurootto@users.noreply.github.com>
2025-06-09 15:20:31 -0300

    up 3

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function baixarPDF() {
-  window.open("https://www.canva.com/design/DAGpaJQYUyU/jeYrTqoB2jjoEEjleMK47g/edit?utm_content=DAGpaJQYUyU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton", "_blank"); // Substitua com o link real
+  window.open("https://www.canva.com/design/DAGpOkBml00/T303TVhtf2nLgHuUiMjsng/edit?utm_content=DAGpOkBml00&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton", "_blank"); // Substitua com o link real
 }
