7d9ca472325aab7aff9a7353040252b6499b626a
rimurootto <160866529+rimurootto@users.noreply.github.com>
2025-06-09 15:12:15 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function baixarPDF() {
-  window.open("https://example.com/livro-de-receitas.pdf", "_blank"); // Substitua com o link real
+  window.open("file:///C:/Users/moresqui.9945/Downloads/_Ingredientes%20%201%20pacote%20de%20Doritos%20(sabor%20a%20escolher,%20preferencialmente%20um%20sabor%20que%20combine%20com%20os%20outros%20ingredientes)%20%201%20abacate%20maduro,%20cortado%20em%20cubos%20%201%20x%C3%ADcara%20de%20tomate%20cereja,%20cortados%20ao.pdf", "_blank"); // Substitua com o link real
 }
