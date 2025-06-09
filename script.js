0361525ee91916fa397dcdfe5f64047c8e7149a8
rimurootto <160866529+rimurootto@users.noreply.github.com>
2025-06-09 15:28:52 -0300

    UP 4

--- script.js
+++
@@ -1,3 +0,0 @@
-function baixarPDF() {
-  window.open("https://www.canva.com/design/DAGpOkBml00/T303TVhtf2nLgHuUiMjsng/edit?utm_content=DAGpOkBml00&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton", "_blank"); // Substitua com o link real
-}
